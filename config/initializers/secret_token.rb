81a5e237134e317c97e271f53d1309242b07c6f1
Matthias Gröbner <m.groebner@me.com>
2013-09-17 17:44:37 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WarblerTest::Application.config.secret_key_base = '7d5df3b9e652cc23c07202a9e62813a91d4fe1b25ac38944ae9f4a116eb583c6ea23d776805ba7b7e28af032616c4cb7419e227a4392dab01cc9cf34c9aca53f'
